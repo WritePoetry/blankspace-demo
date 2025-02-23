1c9d88d91ae17a64a5ae44812ac04be6ec4aa014
Giacomo Secchi <giacomosecchi@gmail.com>
2025-02-23 15:31:18 +0100

    Update header pattern and fix navigation link URL

--- patterns/nav-main.php
+++ patterns/nav-main.php
@@ -5,8 +5,8 @@
  */
 ?>
 
-<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontWeight":"400","fontWeight":"500","textTransform":"uppercase"},"spacing":{"blockGap":"var:preset|spacing|60"}},"fontSize":"small","layout":{"type":"flex","justifyContent":"center"},"setCascadingProperties":true,"ariaLabel":"<?php esc_attr_e( 'Main Menu', 'blankspace-demo' ); ?>"} -->
-    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Blog', 'blankspace-demo' ); ?>","url":"#"} /-->
+<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontWeight":"400","textTransform":"uppercase"},"spacing":{"blockGap":"var:preset|spacing|60"}},"fontSize":"small","layout":{"type":"flex","justifyContent":"center"},"setCascadingProperties":true,"ariaLabel":"<?php esc_attr_e( 'Main Menu', 'blankspace-demo' ); ?>"} -->
+    <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Blog', 'blankspace-demo' ); ?>","url":"<?php echo esc_url( home_url( 'blog/' ) ); ?>"} /-->
     <!-- wp:navigation-link {"label":"<?php esc_html_e( 'About', 'blankspace-demo' ); ?>","url":"#"} /-->
     <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Patterns', 'blankspace-demo' ); ?>","url":"#"} /-->
     <!-- wp:navigation-submenu {"label":"<?php esc_html_e( 'Templates', 'blankspace-demo' ); ?>"} -->
